36463cf4c00bb8a31fd43f7f89beccbdfbe83b25
kishima <kishima7@gmail.com>
2024-12-01 22:46:36 +0900

    update config

--- idf.sh
+++ idf.sh
@@ -6,8 +6,6 @@ source .env
 
 command=$1
 
-MY_ENV_VAR="This is my environment variable"
-
 case "$command" in
   menuconfig)
     docker run -it --rm -v $PWD/$TARGET_PROJECT:/project -w /project -u $(id -u $USER):$(id -g $USER) -e HOME=/tmp espressif/idf:$IDF_VER idf.py menuconfig
@@ -21,14 +19,16 @@ case "$command" in
       -v $PWD/$TARGET_PROJECT:/project -w /project -u $(id -u $USER):$(id -g $USER) -e HOME=/tmp espressif/idf:$IDF_VER idf.py build
     ;;
   work)
-    docker run -it --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp espressif/idf:$IDF_VER bash
+    docker run -it --rm --device=$HOST_USBDEV:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp espressif/idf:$IDF_VER bash
     ;;
   flash)
+    #This command is copied from idf build commnad
+    #"idf.py -p (PORT) flash" doesn't work...
     cmd="/opt/esp/python_env/idf4.4_py3.8_env/bin/python ../opt/esp/idf/components/esptool_py/esptool/esptool.py -p /dev/ttyUSB0 -b 500000 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 80m 0x1000 build/bootloader/bootloader.bin 0x10000 build/partition_table/partition-table.bin 0x20000 build/intdash-esp-camera.bin"
-    docker run --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp --entrypoint="" espressif/idf:$IDF_VER bash -c "$cmd"
+    docker run --rm --device=$HOST_USBDEV:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp --entrypoint="" espressif/idf:$IDF_VER bash -c "$cmd"
     ;;
   monitor)
-    docker run -it --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp espressif/idf:$IDF_VER idf.py monitor
+    docker run -it --rm --device=$HOST_USBDEV:/dev/ttyUSB0 -v $PWD/$TARGET_PROJECT:/project -w /project -e HOME=/tmp espressif/idf:$IDF_VER idf.py monitor
     ;;
   *)
     echo "Command not supported"
